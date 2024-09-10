b40b373acaafb0796afd38614bfd34f11e306d9a
bla-ce <contact.charaud@gmail.com>
2024-09-10 14:28:45 +1000

    get number of words inside query

--- astreus.s
+++ astreus.s
@@ -4,7 +4,44 @@ global _start
 BUFFER_MAX_LEN equ 8192
 
 section .text
+find_number_of_word:
+  ; rdi -> buffer
+  ; rsi -> buffer len
+  ; return value: rax -> count 
+
+  ; prepare stack
+  sub   rsp, 0x10
+  mov   qword [rsp], 0 ; count
+
+loop:
+  mov   al, byte [rdi]
+  cmp   al, 32 ; compare char with space
+  je    add_word
+
+  mov   al, byte [rdi]
+  cmp   al, 0
+  je    end_string
+
+  inc   rdi
+  jmp   loop
+
+add_word:
+  inc   rdi
+  inc   qword [rsp]
+
+  jmp   loop
+
+end_string:
+  inc   qword [rsp]
+
+return:
+  mov   rax, qword [rsp]
+  ; clean stack
+  add   rsp, 0x10
+  ret
+
 _start:
+next_query:
   ; read user input
   mov   rax, 0
   mov   rdi, 0 ; stdin
@@ -17,12 +54,24 @@ _start:
 
   mov   qword [buffer_len], rax
 
-  ; print it back
+  ; split
+  lea   rdi, [buffer]
+  mov   rsi, qword [buffer_len]
+  call  find_number_of_word
+
+  cmp   rax, 3
+  jg    next_query
+
+  cmp   rax, 1
+  jle   next_query 
+
   mov   rax, 1
-  mov   rdi, 1 ; stdout
+  mov   rdi, 1
   lea   rsi, [buffer]
   mov   rdx, qword [buffer_len]
   syscall
+
+  jmp   next_query
 
   ; exit program
   mov   rax, 60
